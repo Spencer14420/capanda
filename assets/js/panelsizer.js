8c9daec717c056955b220f58a02dd61c7c1d377b
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-01-24 23:17:13 -0500

    Fix issue causing header links to scroll to the wrong location

--- assets/js/panelsizer.js
+++ assets/js/panelsizer.js
@@ -1,25 +1,59 @@
 let scrollPoints = [];
 const numPanels = 5 //Total number of panels, including top image, counted starting at 1
-const navHeight = document.querySelector("nav").offsetHeight; //Height of navbar
-
-//Get the height of each panel
-const panelHeight = panel => document.querySelector(`.panel${panel}`).offsetHeight;
 
 const setPositions = () => {
+  let panelHeight = panel => document.querySelector(`.panel${panel}`).offsetHeight;
+  let headerHeight = document.querySelector("nav").clientHeight;
+
   //Set transition points and position of panels
   scrollPoints = [200]; //Position of first transition when scrolling down
   for (let i = 2; i <= numPanels; i++) {
     let scrollPoint = scrollPoints[i-2] + panelHeight(i);
     scrollPoints.push(scrollPoint);
-    document.querySelector(`.panel${i}`).style.top = `-${window.innerHeight/2 - scrollPoints[0] - navHeight}px`; // Places the top of the panel at the middle of the screen (excluding the navbar) when the transition occurs
+    document.querySelector(`.panel${i}`).style.top = `-${window.innerHeight/2 - scrollPoints[0] - headerHeight}px`; // Places the top of the panel at the middle of the screen (excluding the navbar) when the transition occurs
   }
 
   //Set padding for top panel (so text doesn't go under header)
-  const headerHeight = document.querySelector(".navbar").clientHeight;
   document.querySelector(".top-panel").style.paddingTop = `${headerHeight}px`;
+
+  //Position #value element, etc. (for header links)
+  //When the middle of the panel is in the middle of the screen
+  //these elements should be located at the top of the screen
+  let valueTopBottom = (window.innerHeight - document.querySelector("#value-panel").clientHeight + headerHeight)/2;
+  document.querySelector("#value").style.height = `${valueTopBottom}px`;
+  document.querySelector("#value").style.marginTop = `-${valueTopBottom}px`;
+
+  //console.log(`(${window.innerHeight} - ${document.querySelector("#value-panel").clientHeight} + ${headerHeight})/2 = ${(window.innerHeight - document.querySelector("#value-panel").clientHeight + headerHeight)/2}`);
+
+  let leadershipTopBottom = (window.innerHeight - document.querySelector("#leadership-panel").clientHeight + headerHeight)/2;
+  document.querySelector("#leadership").style.height = `${leadershipTopBottom}px`;
+  document.querySelector("#leadership").style.marginTop = `-${leadershipTopBottom}px`;
+
+  let dedicationTopBottom = (window.innerHeight - document.querySelector("#dedication-panel").clientHeight + headerHeight)/2;
+  document.querySelector("#dedication").style.height = `${dedicationTopBottom}px`;
+  document.querySelector("#dedication").style.marginTop = `-${dedicationTopBottom}px`;
+
+  let focusTopBottom = (window.innerHeight - document.querySelector("#focus-panel").clientHeight + headerHeight)/2;
+  document.querySelector("#focus").style.height = `${focusTopBottom}px`;
+  document.querySelector("#focus").style.marginTop = `-${focusTopBottom}px`;
+
+  //Hide the header links if the screen is shorter than the largest panel (in height)
+  //because they don't work if that's the case
+  let largestPanelHeight = Math.max(
+    document.querySelector("#value-panel").clientHeight, 
+    document.querySelector("#leadership-panel").clientHeight, 
+    document.querySelector("#dedication-panel").clientHeight, 
+    document.querySelector("#focus-panel").clientHeight
+    );
+
+    if (window.innerHeight - headerHeight <= largestPanelHeight) {
+      document.querySelector(".navbar-nav").style.display = "none";
+    } else {
+      document.querySelector(".navbar-nav").style.display = "flex";
+    }
 }
 
-$(document).ready(setPositions);
+window.addEventListener('load', setPositions);
 window.onresize = setPositions;
 
 function hideTopPanel() {
