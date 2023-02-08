5e5cfb2e925ac6affd877ad5773e5c1caa9a4230
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-02-08 15:03:37 -0500

    Update panelsizer.js

--- assets/js/panelsizer.js
+++ assets/js/panelsizer.js
@@ -1,28 +1,30 @@
 let scrollPoints = [];
 const numPanels = 5 //Total number of panels, including top image, counted starting at 1
+const panelPrefix = ".panel";
 
 const setPositions = () => {
-  let panelHeight = panel => document.querySelector(`.panel${panel}`).offsetHeight;
-  let headerHeight = document.querySelector("nav").clientHeight;
-
-  let largestPanelHeight = Math.max(
-    document.querySelector("#value-panel").clientHeight, 
-    document.querySelector("#leadership-panel").clientHeight, 
-    document.querySelector("#dedication-panel").clientHeight, 
-    document.querySelector("#focus-panel").clientHeight
-    );
+  const headerHeight = document.querySelector("nav").clientHeight;
+  const panelHeight = panel => document.querySelector(`${panelPrefix+panel}`).offsetHeight;
+  const selectors = ['value', 'leadership', 'dedication', 'focus'];
+  const panels = [
+    document.querySelector(`#${selectors[0]}-panel`), 
+    document.querySelector(`#${selectors[1]}-panel`), 
+    document.querySelector(`#${selectors[2]}-panel`), 
+    document.querySelector(`#${selectors[3]}-panel`)
+  ];
+  const largestPanelHeight = Math.max(...panels.map(panel => panel.clientHeight));
+  const firstTransition = 200; //Position of first transition when scrolling down
+  const extraneousArea = firstTransition + headerHeight;
+  const raiseBy = 400; //Raise each panel by this number of pixels
 
   //Set transition points, position of panels, and visibility of header links
-  let firstTransition = 200;
-  scrollPoints = [firstTransition]; //Position of first transition when scrolling down
-  let extraneousArea = scrollPoints[0] + headerHeight;
-  const raiseBy = 400; //Raise each panel by this number of pixels
+  scrollPoints = [firstTransition]; 
   for (let i = 2; i <= numPanels; i++) {
     let scrollPoint = (scrollPoints[i-2] + panelHeight(i));
     scrollPoints.push(scrollPoint);
   
     let navbar = document.querySelector(".navbar-nav");
-    let panel = document.querySelector(`.panel${i}`);
+    let panel = document.querySelector(`${panelPrefix+i}`);
   
     navbar.style.display = (window.innerHeight - headerHeight <= largestPanelHeight) ? "none" : "flex";
   
@@ -39,7 +41,6 @@ const setPositions = () => {
   //Position #value element, etc. (for header links)
   //When the middle of the panel is in the middle of the screen
   //these elements should be located at the top of the screen
-  const selectors = ['value', 'leadership', 'dedication', 'focus'];
   selectors.forEach(selector => {
     const panel = document.querySelector(`#${selector}-panel`);
     const topBottom = (window.innerHeight - panel.clientHeight + headerHeight) /2;
@@ -49,9 +50,8 @@ const setPositions = () => {
   });
 
   //Set height of the body at the bottom of the last panel
-  let lastPanelBottom = document.querySelector(`.panel${numPanels}`).getBoundingClientRect().bottom + window.scrollY;
+  const lastPanelBottom = document.querySelector(`${panelPrefix+numPanels}`).getBoundingClientRect().bottom + window.scrollY;
   document.querySelector("body").style.height = `${lastPanelBottom}px`;
-  console.log(`${raiseBy + (panelHeight(numPanels) + window.innerHeight - 2*extraneousArea)/2}px`)
   document.querySelector("#footer").style.marginTop = `${(window.innerHeight - 2*extraneousArea)/2}px`;
 }
 
@@ -66,7 +66,7 @@ function toggleTopPanel(state) {
   const topPanel = document.querySelector('.top-panel');
   topPanel.style.opacity = opacity;
 
-  const panel2 = document.querySelector('.panel2');
+  const panel2 = document.querySelector(`${panelPrefix}2`);
   panel2.style.opacity = '0';
   panel2.style.pointerEvents = pointerEvents;
 }
@@ -74,17 +74,17 @@ function toggleTopPanel(state) {
 
 //Changes the text colour of the selected panel
 function textColour(colour, panel) {
-  const text = $(`.panel${panel} p:not(#openingpara), .panel${panel} h1:not(#openingpara)`);
+  const text = document.querySelectorAll(`${panelPrefix+panel} p:not(#openingpara), ${panelPrefix+panel} h1:not(#openingpara)`);
   if (colour === 'white') {
-    text.addClass('text-white');
+    text.forEach(element => element.classList.add('text-white'));
   } else {
-    text.removeClass('text-white');
+    text.forEach(element => element.classList.remove('text-white'));
   }
 }
 
 //Changes the text colour via textColour() and opacity to make the selected panel visible
 function showText(panel) {
-  const panels = document.querySelectorAll('.panel2, .panel3, .panel4, .panel5');
+  const panels = document.querySelectorAll(`${panelPrefix}2, ${panelPrefix}3, ${panelPrefix}4, ${panelPrefix}5`);
   textColour(panel % 2 ? 'white' : 'black', panel + 1);
   panels.forEach(p => p.style.opacity = '0');
   panels[panel - 1].style.opacity = '1';
@@ -92,7 +92,7 @@ function showText(panel) {
 
 //Changes the background colour
 function changeColour(colour) {
-  const panels = document.querySelectorAll('body, .panel2, .panel3, .panel4, .panel5');
+  const panels = document.querySelectorAll(`body, ${panelPrefix}2, ${panelPrefix}3, ${panelPrefix}4, ${panelPrefix}5`);
   toggleTopPanel(0);
   panels.forEach(p => p.style.backgroundColor = colour);
 }
@@ -111,20 +111,20 @@ function headerLinks(highlightLink) {
   }
 }
 
-$(window).scroll(() => {
+window.addEventListener('scroll', function() {
 
   //True if the top of the panel is above the bottom of the header
   let panelTopHigh = [
-    document.querySelector(".panel2").getBoundingClientRect().y < document.querySelector("nav").clientHeight ? true : false,
-    document.querySelector(".panel3").getBoundingClientRect().y < document.querySelector("nav").clientHeight ? true : false,
-    document.querySelector(".panel4").getBoundingClientRect().y < document.querySelector("nav").clientHeight ? true : false
+    document.querySelector(`${panelPrefix}2`).getBoundingClientRect().y < document.querySelector("nav").clientHeight ? true : false,
+    document.querySelector(`${panelPrefix}3`).getBoundingClientRect().y < document.querySelector("nav").clientHeight ? true : false,
+    document.querySelector(`${panelPrefix}4`).getBoundingClientRect().y < document.querySelector("nav").clientHeight ? true : false
   ];
 
   //True if the bottom of the panel is above the bottom of the screen
   let panelBottomHigh = [
-    document.querySelector(".panel2").getBoundingClientRect().bottom < window.innerHeight ? true : false,
-    document.querySelector(".panel3").getBoundingClientRect().bottom < window.innerHeight ? true : false,
-    document.querySelector(".panel4").getBoundingClientRect().bottom < window.innerHeight ? true : false
+    document.querySelector(`${panelPrefix}2`).getBoundingClientRect().bottom < window.innerHeight ? true : false,
+    document.querySelector(`${panelPrefix}3`).getBoundingClientRect().bottom < window.innerHeight ? true : false,
+    document.querySelector(`${panelPrefix}4`).getBoundingClientRect().bottom < window.innerHeight ? true : false
   ];
 
   const screenTop = window.scrollY;
