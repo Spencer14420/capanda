bdd37d10b36979b188968a390d22360da5f788e1
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-02-07 22:46:50 -0500

    shorten

--- assets/js/panelsizer.js
+++ assets/js/panelsizer.js
@@ -20,18 +20,17 @@ const setPositions = () => {
   for (let i = 2; i <= numPanels; i++) {
     let scrollPoint = (scrollPoints[i-2] + panelHeight(i));
     scrollPoints.push(scrollPoint);
-
-    if (window.innerHeight - headerHeight <= largestPanelHeight) {
-      document.querySelector(".navbar-nav").style.display = "none";
-      document.querySelector(`.panel${i}`).style.top = `${extraneousArea - window.innerHeight*0.9}px`; //Position the panels near the top of the screen if the largest panel would be placed too high
-    } else {
-      document.querySelector(".navbar-nav").style.display = "flex";
-      document.querySelector(`.panel${i}`).style.top = `-${raiseBy + (panelHeight(i) + window.innerHeight - 2*extraneousArea)/2}px`; //Place the middle of the text in the middle of the screen
-      if (i === 2) {
-        document.querySelector(`.panel${i}`).style.top = `-${(panelHeight(i) + window.innerHeight - 2*extraneousArea)/2}px`;
-      }
-    }
+  
+    let navbar = document.querySelector(".navbar-nav");
+    let panel = document.querySelector(`.panel${i}`);
+  
+    navbar.style.display = (window.innerHeight - headerHeight <= largestPanelHeight) ? "none" : "flex";
+  
+    panel.style.top = (window.innerHeight - headerHeight <= largestPanelHeight) ?
+      `${extraneousArea - window.innerHeight*0.9}px` : //Position the panels near the top of the screen if the largest panel would be placed too high
+      `-${(i === 2 ? 0 : raiseBy) + (panelHeight(i) + window.innerHeight - 2*extraneousArea)/2}px`; //Place the middle of the text in the middle of the screen
   }
+  
  scrollPoints = [firstTransition, scrollPoints[1] - raiseBy, scrollPoints[2] - raiseBy, scrollPoints[3] - raiseBy, scrollPoints[4] - raiseBy]
 
   //Set padding for top panel (so text doesn't go under header)
@@ -41,7 +40,6 @@ const setPositions = () => {
   //When the middle of the panel is in the middle of the screen
   //these elements should be located at the top of the screen
   const selectors = ['value', 'leadership', 'dedication', 'focus'];
-
   selectors.forEach(selector => {
     const panel = document.querySelector(`#${selector}-panel`);
     const topBottom = (window.innerHeight - panel.clientHeight + headerHeight) /2;
@@ -54,22 +52,21 @@ const setPositions = () => {
 window.addEventListener('load', setPositions);
 window.onresize = setPositions;
 
-function hideTopPanel() {
-  $('.top-panel').css('opacity', '0');
-  $('.panel2').css({
-    'opacity': '0',
-    'pointer-events': 'auto',
-  });
+//toggleTopPanel(0) hides the top panel and toggleTopPanel(1) makes it visible
+function toggleTopPanel(state) {
+  const opacity = state === 1 ? '1' : '0';
+  const pointerEvents = state === 1 ? 'none' : 'auto';
+
+  const topPanel = document.querySelector('.top-panel');
+  topPanel.style.opacity = opacity;
+
+  const panel2 = document.querySelector('.panel2');
+  panel2.style.opacity = '0';
+  panel2.style.pointerEvents = pointerEvents;
 }
 
-function showTopPanel() {
-  $('.top-panel').css('opacity', '1');
-  $('.panel2').css({
-    'opacity': '0',
-    'pointer-events': 'none',
-  });
-}
 
+//Changes the text colour of the selected panel
 function textColour(colour, panel) {
   const text = $(`.panel${panel} p:not(#openingpara), .panel${panel} h1:not(#openingpara)`);
   if (colour === 'white') {
@@ -79,38 +76,19 @@ function textColour(colour, panel) {
   }
 }
 
+//Changes the text colour via textColour() and opacity to make the selected panel visible
 function showText(panel) {
-  if (panel === 1) {
-    textColour('white', 2);
-    $('.panel3, .panel4, .panel5').css('opacity', '0');
-    $('.panel2').css('opacity', '1');
-  } else if (panel === 2) {
-    textColour('black', 4);
-    $('.panel2, .panel4, .panel5').css('opacity', '0');
-    $('.panel3').css('opacity', '1');
-  } else if (panel === 3) {
-    textColour('white', 4);
-    $('.panel2, .panel3, .panel5').css('opacity', '0');
-    $('.panel4').css('opacity', '1');
-  } else if (panel === 4) {
-    textColour('black', 5);
-    $('.panel2, .panel3, .panel4').css('opacity', '0');
-    $('.panel5').css('opacity', '1');
-  }
+  const panels = document.querySelectorAll('.panel2, .panel3, .panel4, .panel5');
+  textColour(panel % 2 ? 'white' : 'black', panel + 1);
+  panels.forEach(p => p.style.opacity = '0');
+  panels[panel - 1].style.opacity = '1';
 }
 
+//Changes the background colour
 function changeColour(colour) {
-  const panels = $('body, .panel2, .panel3, .panel4, .panel5');
-  hideTopPanel();
-  if (colour === 'blue') {
-    panels.css('background-color', '#0e2c57');
-  } else if (colour === 'white') {
-    panels.css('background-color', 'white');
-  } else if (colour === 'black') {
-    panels.css('background-color', '#14171c');
-  } else if (colour === 'grey') {
-    panels.css('background-color', '#ededed');
-  }
+  const panels = document.querySelectorAll('body, .panel2, .panel3, .panel4, .panel5');
+  toggleTopPanel(0);
+  panels.forEach(p => p.style.backgroundColor = colour);
 }
 
 // Highlight header links based on which panel is visible
@@ -146,10 +124,10 @@ $(window).scroll(() => {
   const screenTop = window.scrollY;
 
   if (screenTop < scrollPoints[0]) {
-    showTopPanel();
+    toggleTopPanel(1);
     headerLinks(-1);
   } else if (screenTop >= scrollPoints[0] && screenTop < scrollPoints[1]) {
-    changeColour('blue');
+    changeColour('#0e2c57');
     showText(1);
     headerLinks(0);
   } else if (screenTop >= scrollPoints[1] && screenTop < scrollPoints[2]) {
@@ -157,11 +135,11 @@ $(window).scroll(() => {
     showText(2);
     headerLinks(1);
   } else if (screenTop >= scrollPoints[2] && screenTop < scrollPoints[3]) {
-    changeColour('black');
+    changeColour('#14171c');
     showText(3);
     headerLinks(2);
   } else if (screenTop >= scrollPoints[3]) {
-    changeColour('grey');
+    changeColour('#ededed');
     showText(4);
     headerLinks(3);
   }
