725b02f7cc209815a6b9bacd0c183a55689ba46c
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-02-11 13:45:49 -0500

    Revert "Sanitize input"
    This reverts commit 5292b6222ab8e9e716f3a7e9e60a0f39dbf8d360.

--- assets/js/email-handler.php
+++ assets/js/email-handler.php
@@ -1,9 +1,9 @@
 <?php
     if (isset($_POST["email"]) && isset($_POST["message"])) {
-        $name = htmlspecialchars($_POST["name"]);
-        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $name = $_POST["name"];
+        $email = $_POST["email"];
         $headers = "From: admin@capanda.ca" . "\r\n" . "Reply-To: $email";
-        $message = "From: " . $name . " (" . $email . ")\n\nMessage:\n" . wordwrap(htmlspecialchars($_POST["message"]), 70);
+        $message = "From: " . $name . " (" . $email . ")\n\nMessage:\n" . wordwrap($_POST["message"], 70);
 
         mail("info@capanda.ca", "New message sent from capanda.ca", $message, $headers);
     }
