b6cf16fb99b6d5539e2e73d2c8637b7f913fc8b7
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-02-12 12:00:18 -0500

    Remove jquery from email-handler.js

--- assets/js/email-handler.js
+++ assets/js/email-handler.js
@@ -1,37 +1,48 @@
-function isEmail(email) {
-  const regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
-}
+const isEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
-function messageSuccess() {
-  $('#name').val('');
-  $('#email').val('');
-  $('#message').val('');
-  $('#messagecancel').click();
-  $('#success').modal('show');
-}
+const messageSuccess = () => {
+  ["#name", "#email", "#message"].forEach((element) => {
+    document.querySelector(element).value = "";
+  });
+  document.querySelector("#messagecancel").click();
+  document.querySelector("#success").style.display = "block";
+};
 
-$('#sendmessage').click(() => {
-  if (!$('#email').val() || !isEmail($('#email').val())) {
-    $('#message-alert').html('Please enter an email address');
-    $('#message-alert').show();
-  } else if (!$('#message').val()) {
-    $('#message-alert').html('Please enter a message');
-    $('#message-alert').show();
+const sendMessage = async (data) => {
+  try {
+    const response = await fetch("assets/js/email-handler.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (response.ok) {
+      messageSuccess();
+    }
+  } catch (error) {
+    console.error("Error sending message", error);
+  }
+};
+
+const messageAlert = document.querySelector("#message-alert");
+
+document.querySelector("#sendmessage").addEventListener("click", () => {
+  const email = document.querySelector("#email").value;
+  const message = document.querySelector("#message").value;
+
+  if (!email || !isEmail(email)) {
+    messageAlert.innerHTML = "Please enter a valid email address";
+    messageAlert.style.display = "block";
+  } else if (!message) {
+    messageAlert.innerHTML = "Please enter a message";
+    messageAlert.style.display = "block";
   } else {
     const data = {
-      name: $('#name').val(),
-      email: $('#email').val(),
-      message: $('#message').val(),
+      name: document.querySelector("#name").value,
+      email,
+      message,
     };
-
-    $.ajax({
-      type: 'POST',
-      url: 'assets/js/email-handler.php',
-      data,
-      success: () => {
-        messageSuccess();
-      },
-    });
+    sendMessage(data);
   }
 });
