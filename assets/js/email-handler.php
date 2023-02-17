5dfc1f0208279230cbe657cbd17bf5fcf774721d
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-02-17 13:57:39 -0500

    Fix my errors

--- assets/js/email-handler.php
+++ assets/js/email-handler.php
@@ -4,16 +4,14 @@
         $name = $_POST["name"];
         $email = $_POST["email"];
         $headers = "From: admin@capanda.ca" . "\r\n" . "Reply-To: $email";
-        $message = "From: {$name} ({$email})\n\nMessage:\n{$wordwrap($_POST["message"], 70)}";
+        $message = "From: {$name} ({$email})\n\nMessage:\n". wordwrap($_POST["message"], 70);
 
         mail("info@capanda.ca", "Message from {$name} via capanda.ca", $message, $headers);
 
         //Send confirmation email to sender
-        $name = $_POST["name"];
-        $email = $_POST["email"];
         $headers = "From: info@capanda.ca";
-        $message = "Dear, {$name} ({$email})\n\n Your message (shown below) has been recieved. We will get back to you as soon as possible\n\n Sincerely,\nCAPANDA\n\nYour message: {$wordwrap($_POST["message"], 70)}";
+        $message = "Dear {$name} ({$email}),\n\nYour message (shown below) has been recieved. We will get back to you as soon as possible.\n\nSincerely,\nCAPANDA\n\nYour message:\n". wordwrap($_POST["message"], 70);
 
-        mail("info@capanda.ca", "New message sent from capanda.ca", $message, $headers);
+        mail($email, "Your message to CAPANDA has been recieved", $message, $headers);
     }
 ?>
