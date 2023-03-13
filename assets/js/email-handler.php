d0cbc68b6cf7b095ddd48a00324d21ac71a319ed
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-03-13 16:07:22 -0400

    Add disclaimer to email, load bootstrap from CDN and defer email-handler.js

--- assets/js/email-handler.php
+++ assets/js/email-handler.php
@@ -1,17 +1,16 @@
 <?php
-    if (isset($_POST["email"]) && isset($_POST["message"])) {
-        //Send email to info@capanda.ca
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $headers = "From: admin@capanda.ca\r\nReply-To: $email";
-        $message = "From: {$name} ({$email})\n\nMessage:\n". wordwrap($_POST["message"], 70);
+if (isset($_POST["email"]) && isset($_POST["message"])) {
+    //Send email to info@capanda.ca
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $headers = "From: admin@capanda.ca\r\nReply-To: $email";
+    $message = "From: {$name} ({$email})\n\nMessage:\n" . wordwrap($_POST["message"], 70);
 
-        mail("info@capanda.ca", "Message from {$name} via capanda.ca", $message, $headers);
+    mail("info@capanda.ca", "Message from {$name} via capanda.ca", $message, $headers);
 
-        //Send confirmation email to sender
-        $headers = "From: info@capanda.ca";
-        $message = "Dear {$name} ({$email}),\n\nYour message (shown below) has been recieved. We will get back to you as soon as possible.\n\nSincerely,\nCAPANDA\n\nYour message:\n". wordwrap($_POST["message"], 70);
+    //Send confirmation email to sender
+    $headers = "From: info@capanda.ca";
+    $message = "Dear {$name} ({$email}),\n\nYour message (shown below) has been recieved. We will get back to you as soon as possible.\n\nSincerely,\nCAPANDA\n\nPlease note: This message was sent referencing your email address - If this was not you, please be aware of the use of your email address.\n\nYour message:\n" . wordwrap($_POST["message"], 70);
 
-        mail($email, "Your message to CAPANDA has been recieved", $message, $headers);
-    }
-?>
+    mail($email, "Your message to CAPANDA has been recieved", $message, $headers);
+}
