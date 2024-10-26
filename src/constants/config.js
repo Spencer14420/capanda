e4d9e58a748288f5daa4a5b9d411f65ff6570b67
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-10-26 11:36:58 -0400

    Config comments

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,11 +1,11 @@
 export const CONFIG = {
     numPanels: 5,
-    firstTransition: 200,
-    topHighOffset: 50,
-    bottomHighOffset: 100,
-    minTopBottomSpace: 100,
-    shortScreenAddition: 300,
-    largeScreenAddition: 50,
+    firstTransition: 200, //Minimum amount to scroll before the first transision occurs.
+    topHighOffset: 50, //Minimum amount to scroll before each subsequent transition occurs
+    bottomHighOffset: 100, //Minimum amount of blank space below each panel before transition can occur
+    minTopBottomSpace: 100, //Minimum space required above and below the tallest panel to consider the screen "large"
+    shortScreenAddition: 300, //Pushes the panels down by this amount on short screens
+    largeScreenAddition: 50, //Pushes the panels down by this amount on large screens
     panelPrefix: ".panel",
     colors: {
       blue: "#0e2c57",
