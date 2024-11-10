67b73b756a8c891a61934e1682f3a629cb84667c
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-11-09 21:29:36 -0500

    Use a config file to set certain options for the email form + minor adjustments

---
+++ assets/js/email-config.php
@@ -0,0 +1,6 @@
+<?php
+$fromEmail = "admin@nm.capanda.ca";
+$mailboxEmail = "admin@nm.capanda.ca";
+$replyToEmail = "admin@capanda.ca";
+$siteDomain = "capanda.ca";
+$siteName = "CAPANDA";
--- assets/js/email-handler.php
+++ assets/js/email-handler.php
@@ -1,15 +1,17 @@
 <?php
+require_once __DIR__ . "/email-config.php";
+
 if (isset($_POST["email"]) && isset($_POST["message"])) {
-    $name = $_POST["name"];
+    $name = !empty($_POST["name"]) ? $_POST["name"] : "somebody";
     $email = $_POST["email"];
-    $headers = "From: admin@nm.capanda.ca\r\nReply-To: $email";
+    $headers = "From: {$siteName} <{$fromEmail}>\r\nReply-To: $email";
     $message = "From: {$name} ({$email})\n\nMessage:\n" . wordwrap($_POST["message"], 70);
 
-    mail("admin@capanda.ca", "Message from {$name} via capanda.ca", $message, $headers);
+    mail($mailboxEmail, "Message from {$name} via {$siteDomain}", $message, $headers);
 
     //Send confirmation email to sender
-    $headers = "From: admin@nm.capanda.ca";
-    $message = "Dear {$name} ({$email}),\n\nYour message (shown below) has been recieved. We will get back to you as soon as possible.\n\nSincerely,\nCAPANDA\n\nPlease note: This message was sent referencing your email address - If this was not you, please be aware of the use of your email address.\n\nYour message:\n" . wordwrap($_POST["message"], 70);
+    $headers = $headers = "From: {$siteName} <{$fromEmail}>\r\nReply-To: $replyToEmail";
+    $message = "Dear {$name} ({$email}),\n\nYour message (shown below) has been recieved. We will get back to you as soon as possible.\n\nSincerely,\n{$siteName}\n\nPlease note: This message was sent to the email address provided in our contact form. If you did not enter your email, please disregard this message.\n\nYour message:\n" . wordwrap($_POST["message"], 70);
 
-    mail($email, "Your message to CAPANDA has been recieved", $message, $headers);
+    mail($email, "Your message to {$siteName} has been recieved", $message, $headers);
 }
