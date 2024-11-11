9a7bbc9acbbdf00fce18c471b9143096bd1b47e9
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-11-11 15:13:46 -0500

    Fix typos

--- dist/php/email-handler.php
+++ dist/php/email-handler.php
@@ -35,7 +35,7 @@ if (!$messageSent) {
 
 //Send confirmation email to sender
 $headers = "From: {$siteName} <{$fromEmail}>\r\nReply-To: $replyToEmail";
-$message = "Dear {$name} ({$email}),\n\nYour message (shown below) has been recieved. We will get back to you as soon as possible.\n\nSincerely,\n{$siteName}\n\nPlease note: This message was sent to the email address provided in our contact form. If you did not enter your email, please disregard this message.\n\nYour message:\n" . wordwrap($_POST["message"], 70);
-mail($email, "Your message to {$siteName} has been recieved", $message, $headers);
+$message = "Dear {$name} ({$email}),\n\nYour message (shown below) has been received. We will get back to you as soon as possible.\n\nSincerely,\n{$siteName}\n\nPlease note: This message was sent to the email address provided in our contact form. If you did not enter your email, please disregard this message.\n\nYour message:\n" . wordwrap($_POST["message"], 70);
+mail($email, "Your message to {$siteName} has been received", $message, $headers);
 
 echo json_encode(['status' => 'success']);
