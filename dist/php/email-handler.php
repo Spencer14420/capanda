4a81ec5211ff15a22d7e31770eb191261369c335
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-11-14 16:01:00 -0500

    Add captcha verification

--- dist/php/email-handler.php
+++ dist/php/email-handler.php
@@ -1,8 +1,44 @@
 <?php
 require_once __DIR__ . "/../../vendor/autoload.php";
+require_once __DIR__ . "/turnstile-config.php";
 
 use spencer14420\PhpEmailHandler\EmailHandler;
 
+//Cloudflare Turnstile verification
+$remote_addr = $_SERVER['REMOTE_ADDR'];
+$cf_url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+$token = $_POST['turnstileToken'];
+
+$data = array(
+    "secret" => $turnstileSecret,
+    "response" => $token,
+    "remoteip" => $remote_addr
+);
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $cf_url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($curl);
+
+if (curl_errno($curl)) {
+    http_response_code(500);
+    echo json_encode(['status' => 'error', 'message' => 'Error: Could not verify CAPTCHA']);
+    exit();
+}
+
+$response = json_decode($response, true);
+
+if ($response['error-codes'] && count($response['error-codes']) > 0) {
+    http_response_code(500);
+    echo json_encode(['status' => 'error', 'message' => "Error: Could not verify CAPTCHA", 'turnstileError' => $response['error-codes']]);
+    exit();
+}
+
+curl_close($curl);
+
+//Send the message
 try {
     $emailHandler = new EmailHandler(__DIR__ . "/email-config.php");
     $emailHandler->handleRequest();
