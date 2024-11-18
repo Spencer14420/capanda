c01836b5e323f51c706e1650134086ab33070d4a
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-11-18 11:54:46 -0500

    Update sp-php-email-handler and use its built in CAPTCHA and CSRF verification (#59)

--- dist/php/email-config.php
+++ dist/php/email-config.php
@@ -1,6 +1,12 @@
 <?php
+require_once __DIR__ . "/turnstile-config.php";
+
 $fromEmail = "admin@nm.capanda.ca";
 $mailboxEmail = "admin@nm.capanda.ca";
 $replyToEmail = "admin@capanda.ca";
 $siteDomain = "capanda.ca";
 $siteName = "CAPANDA";
+$captchaToken = $_POST['turnstileToken'];
+$captchaVerifyURL = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+$checkCsrf = true;
+$csrfToken = $_POST["tokenInputToken"];
