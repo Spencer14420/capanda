6468a35b02769077cc9f1bf8a717a45cb07c4a61
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-11-21 10:54:14 -0500

    Allow handling of anchor tags

--- src/classes/ScrollButton.ts
+++ src/classes/ScrollButton.ts
@@ -14,11 +14,25 @@ export class ScrollButton {
     this.scrollToElement = scrollToElement;
     this.scrollPosition = scrollPosition;
 
-    this.clickElement.addEventListener("click", () => {
-      this.scrollToElement.scrollIntoView({
-        behavior: "smooth",
-        block: scrollPosition,
-      });
+    this.clickElement.addEventListener("click", (event) =>
+      this.handleClick(event),
+    );
+  }
+
+  private handleClick(event: MouseEvent): void {
+    // Prevent the default behavior for anchor tags,
+    // unless the user intends to open the link in a new tab
+    // by holding the Meta key (cmd on MacOS) or the Ctrl key
+    if (this.clickElement instanceof HTMLAnchorElement) {
+      if (event.metaKey || event.ctrlKey) {
+        return;
+      }
+      event.preventDefault();
+    }
+
+    this.scrollToElement.scrollIntoView({
+      behavior: "smooth",
+      block: this.scrollPosition,
     });
   }
 }
