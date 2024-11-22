2c0520f153dde867f7f9324878713ef4073de0e2
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-11-22 13:39:10 -0500

    Adjust spacing and settings to ensure large screens are not considered "small", which pushes the panels down too much (#66)

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -15,9 +15,9 @@ export const CONFIG: Config = {
   firstTransition: 200, // Minimum amount to scroll before the first transition occurs.
   topHighOffset: 50, // Minimum amount to scroll before each subsequent transition occurs.
   bottomHighOffset: 100, // Minimum amount of blank space below each panel before transition can occur.
-  minTopBottomSpace: 100, // Minimum space required above and below the tallest panel to consider the screen "large".
+  minTopBottomSpace: 80, // Minimum space required above and below the tallest panel to consider the screen "large".
   shortScreenAddition: 300, // Pushes the panels down by this amount on short screens.
-  largeScreenAddition: 0, // Pushes the panels down by this amount on large screens.
+  largeScreenAddition: 25, // Pushes the panels down by this amount on large screens.
   panelPrefix: ".panel",
   colors: {
     blue: "#0e2c57",
