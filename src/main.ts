9c028be60609cc3d72f32dae620f995a89d8a2e9
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-12-02 08:38:06 -0500

    Install GSAP and disable some things for testing

--- src/main.ts
+++ src/main.ts
@@ -68,59 +68,59 @@ const panelManager = new PanelManager();
 
 // Set panel positions when the page loads or the window is resized
 window.addEventListener("load", () => {
-  panelManager.setPositions();
+  //panelManager.setPositions();
 });
 
 const debouncedResize = Utils.debounce(function () {
-  panelManager.setPositions();
+  //panelManager.setPositions();
   Utils.updateTurnstileWidget();
 }, 200);
 window.addEventListener("resize", debouncedResize);
 
 //Dynamically update panel styles and UI elements based on the scroll position.
-window.addEventListener("scroll", () => {
-  const panelTopHigh: boolean[] = [];
-  const panelBottomHigh: boolean[] = [];
-  const navElement = document.querySelector("nav") as HTMLElement | null;
+// window.addEventListener("scroll", () => {
+//   const panelTopHigh: boolean[] = [];
+//   const panelBottomHigh: boolean[] = [];
+//   const navElement = document.querySelector("nav") as HTMLElement | null;
 
-  for (let i = 2; i <= derivedValues.numPanels - 1; i++) {
-    const panel = panelManager.getPanel(i);
-    if (!panel || !navElement) {
-      continue;
-    }
+//   for (let i = 2; i <= derivedValues.numPanels - 1; i++) {
+//     const panel = panelManager.getPanel(i);
+//     if (!panel || !navElement) {
+//       continue;
+//     }
 
-    panelTopHigh.push(
-      panel.element.getBoundingClientRect().y <
-        navElement.clientHeight - CONFIG.topHighOffset,
-    );
+//     panelTopHigh.push(
+//       panel.element.getBoundingClientRect().y <
+//         navElement.clientHeight - CONFIG.topHighOffset,
+//     );
 
-    panelBottomHigh.push(
-      panel.element.getBoundingClientRect().bottom <
-        window.innerHeight - CONFIG.bottomHighOffset,
-    );
-  }
+//     panelBottomHigh.push(
+//       panel.element.getBoundingClientRect().bottom <
+//         window.innerHeight - CONFIG.bottomHighOffset,
+//     );
+//   }
 
-  const screenTop: number = window.scrollY;
+//   const screenTop: number = window.scrollY;
 
-  //Based on the scroll position, determine which panel to highlight and what colors to set.
-  if (screenTop < CONFIG.firstTransition) {
-    UIManager.toggleTopPanel(1);
-    UIManager.headerLinks(-1);
-  } else if (screenTop >= CONFIG.firstTransition && !panelTopHigh[0]) {
-    UIManager.setPanelBackgroundColour(CONFIG.colors.blue);
-    UIManager.showText(1);
-    UIManager.headerLinks(0);
-  } else if (panelTopHigh[0] && !panelTopHigh[1] && panelBottomHigh[0]) {
-    UIManager.setPanelBackgroundColour(CONFIG.colors.white);
-    UIManager.showText(2);
-    UIManager.headerLinks(1);
-  } else if (panelTopHigh[1] && !panelTopHigh[2] && panelBottomHigh[1]) {
-    UIManager.setPanelBackgroundColour(CONFIG.colors.black);
-    UIManager.showText(3);
-    UIManager.headerLinks(2);
-  } else if (panelTopHigh[2] && panelBottomHigh[2]) {
-    UIManager.setPanelBackgroundColour(CONFIG.colors.white);
-    UIManager.showText(4);
-    UIManager.headerLinks(3);
-  }
-});
+//   //Based on the scroll position, determine which panel to highlight and what colors to set.
+//   if (screenTop < CONFIG.firstTransition) {
+//     UIManager.toggleTopPanel(1);
+//     UIManager.headerLinks(-1);
+//   } else if (screenTop >= CONFIG.firstTransition && !panelTopHigh[0]) {
+//     UIManager.setPanelBackgroundColour(CONFIG.colors.blue);
+//     UIManager.showText(1);
+//     UIManager.headerLinks(0);
+//   } else if (panelTopHigh[0] && !panelTopHigh[1] && panelBottomHigh[0]) {
+//     UIManager.setPanelBackgroundColour(CONFIG.colors.white);
+//     UIManager.showText(2);
+//     UIManager.headerLinks(1);
+//   } else if (panelTopHigh[1] && !panelTopHigh[2] && panelBottomHigh[1]) {
+//     UIManager.setPanelBackgroundColour(CONFIG.colors.black);
+//     UIManager.showText(3);
+//     UIManager.headerLinks(2);
+//   } else if (panelTopHigh[2] && panelBottomHigh[2]) {
+//     UIManager.setPanelBackgroundColour(CONFIG.colors.white);
+//     UIManager.showText(4);
+//     UIManager.headerLinks(3);
+//   }
+// });
